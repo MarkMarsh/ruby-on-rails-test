12c7c04c8b70dd65b10578f2e5021594a66cbd2d
Mark Marsh <git@flambe.me.uk>
2020-08-10 18:25:47 +0100

    modified table create and ran rails db:migrate

--- db/migrate/20200810171426_create_file_stats.rb
+++ db/migrate/20200810171426_create_file_stats.rb
@@ -1,11 +1,11 @@
 class CreateFileStats < ActiveRecord::Migration[6.0]
   def change
     create_table :file_stats do |t|
-      t.string :username
-      t.text :filename
-      t.boolean :most_status
-      t.boolean :least_status
-      t.boolean :palindrome_status
+      t.string :username, null: false
+      t.text :filename, null: false
+      t.boolean :most_status, null: false, default: false
+      t.boolean :least_status, null: false, default: false
+      t.boolean :palindrome_status, null: false, default: false
 
       t.timestamps
     end
