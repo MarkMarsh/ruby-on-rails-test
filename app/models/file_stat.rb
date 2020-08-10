62f18c75baf505006454a79beec8ab388f69964b
Mark Marsh <git@flambe.me.uk>
2020-08-10 21:20:02 +0100

    added beginnings of sidekiq processing

--- app/models/file_stat.rb
+++ app/models/file_stat.rb
@@ -1,7 +1,12 @@
 class FileStat < ApplicationRecord
   before_save :set_user
+  after_save :process_file
 
   def set_user
     self.username = "Eric"
+  end
+
+  def process_file
+    HardWorker.perform_async(self.filename, 5) 
   end
 end
