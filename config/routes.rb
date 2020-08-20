9679430521d6f07ae3db37319b6333292ff1b7b1
Mark Marsh <git@flambe.me.uk>
2020-08-20 13:40:42 +0100

    WIP pause / cancel functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   root to: 'file_stats#index'
 
-  #if Rails.env.development?
+  if Rails.env.development?
     mount Sidekiq::Web => '/sidekiq'
-  #end
+  end
   
   resources :file_stats
+
+  put '/file_stats/:id/pause', to: "file_stats#pause", as: 'pause_file_stat'
+  put '/file_stats/:id/unpause', to: "file_stats#unpause", as: 'unpause_file_stat'
+  put '/file_stats/:id/cancel', to: "file_stats#cancel", as: 'cancel_file_stat'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
